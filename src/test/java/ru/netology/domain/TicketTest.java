6848a16755f726f8c839a05252363b2d197fb996
DashaSlesareva <zalezka@yandex.ru>
2022-08-29 11:04:12 +0300

    Tests added

--- src/test/java/ru/netology/domain/TicketTest.java
+++ src/test/java/ru/netology/domain/TicketTest.java
@@ -1,6 +1,7 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicketTest {
@@ -24,7 +25,7 @@ public class TicketTest {
     }
 
     @Test
-    public void repoRemoveByIdTest(){
+    public void repoRemoveByIdTest() {
         TicketRepo repo = new TicketRepo();
         repo.add(ticket1);
         repo.add(ticket2);
@@ -37,15 +38,16 @@ public class TicketTest {
 
     //тестирую CompareTo
     @Test
-    public void ticketComareToTest(){
+    public void ticketComareToTest() {
         assertEquals(ticket1.compareTo(ticket2), -1);
         assertEquals(ticket3.compareTo(ticket2), 1);
         assertEquals(ticket1.compareTo(ticket1), 0);
     }
 
     //Тестирую менеждер
+    //Тестрирую метод поиска, когда находится несколько товаров
     @Test
-    public void managerFindAllTest(){
+    public void managerFindSeveralTicketsTest() {
         TicketRepo repo = new TicketRepo();
         TicketManager manager = new TicketManager(repo);
         manager.add(ticket1);
@@ -53,12 +55,40 @@ public class TicketTest {
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
-        Ticket[] actual = manager.findAll("SVO","KZN");
-        Ticket[] expected = {ticket4,ticket1,ticket5};
+        Ticket[] actual = manager.findAll("SVO", "KZN");
+        Ticket[] expected = {ticket4, ticket1, ticket5};
         assertArrayEquals(actual, expected);
     }
 
+    //Тестрирую метод поиска, когда находится один товар
+    @Test
+    public void managerFindSingleTicketTest() {
+        TicketRepo repo = new TicketRepo();
+        TicketManager manager = new TicketManager(repo);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        Ticket[] actual = manager.findAll("MDE", "ICN");
+        Ticket[] expected = {ticket3};
+        assertArrayEquals(actual, expected);
+    }
 
+    //Тестрирую метод поиска, когда находится 0 товаров
+    @Test
+    public void managerFindZeroTicketsTest() {
+        TicketRepo repo = new TicketRepo();
+        TicketManager manager = new TicketManager(repo);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        Ticket[] actual = manager.findAll("MDE", "KZN");
+        Ticket[] expected = {};
+        assertArrayEquals(actual, expected);
+    }
 
 
 }
